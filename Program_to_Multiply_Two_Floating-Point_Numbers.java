cc0a76673da52170dc728b75ed612167af742777
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-10-16 11:46:49 +0530

    import java.util.Scanner; class multiplyExp1 {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float a,b;         a=sc.nextFloat();         b=sc.nextFloat();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+class multiplyExp1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a,b;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.format("%.2f",a*b);
+    }
+}
+
+
+

24aec29f7360e561070cbf01c99e923252dc2aea
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-10-16 14:45:19 +0530

    import java.util.Scanner; class raindrop {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         String s=String.valueOf(n);         if(n%3==0)         {             System.out.print("Pling");         }         if(n%5==0)         {             System.out.print("Plang");         }         if(n%7==0)         {             System.out.print("Plong");         }         if(n%3!=0 && n%5!=0 && n%7!=0)         {             System.out.print(s);         }     } }

---
+++ RainDrop.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Scanner;
+class raindrop
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=String.valueOf(n);
+        if(n%3==0)
+        {
+            System.out.print("Pling");
+        }
+        if(n%5==0)
+        {
+            System.out.print("Plang");
+        }
+        if(n%7==0)
+        {
+            System.out.print("Plong");
+        }
+        if(n%3!=0 && n%5!=0 && n%7!=0)
+        {
+            System.out.print(s);
+        }
+    }
+}
+
+
+

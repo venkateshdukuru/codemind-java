be2a6ec88bef6734a8f2608c4923fd570fe828e5
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-10-16 11:28:37 +0530

    import java.util.*; class Sampleexp1 {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String line=sc.nextLine();         String input[]=line.split(":");         int h=Integer.parseInt(input[0]);         int m=Integer.parseInt(input[1]);         double a=Math.abs((h*30)-(11*m)/2.0);         if(a<360-a)            if(a>(int)a)             System.out.printf("%.1f",a);            else             System.out.printf("%.1f",a);         else            if(360-a>(int)(360-a))                  System.out.printf("%.1f",360-a);            else                  System.out.format("%.1f",(360-a));     } }

---
+++ Angle_Between_Hands.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sampleexp1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        String input[]=line.split(":");
+        int h=Integer.parseInt(input[0]);
+        int m=Integer.parseInt(input[1]);
+        double a=Math.abs((h*30)-(11*m)/2.0);
+        if(a<360-a)
+           if(a>(int)a)
+            System.out.printf("%.1f",a);
+           else
+            System.out.printf("%.1f",a);
+        else
+           if(360-a>(int)(360-a))
+                 System.out.printf("%.1f",360-a);
+           else
+                 System.out.format("%.1f",(360-a));
+    }
+}

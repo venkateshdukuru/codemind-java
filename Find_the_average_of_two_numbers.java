b0a1ac0a1ba60fd22a7aca28620aa22e6d055b71
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-10-16 11:43:19 +0530

    import java.util.Scanner; class SumExp1 {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a,b,sum=0.0000;         a=sc.nextDouble();         b=sc.nextDouble();         sum=(a+b)/2;         System.out.format("%.4f",sum);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class SumExp1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a,b,sum=0.0000;
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        sum=(a+b)/2;
+        System.out.format("%.4f",sum);
+        
+    }
+}
